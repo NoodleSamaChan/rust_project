37697d3fcd7df235140cd0bb95dbd69db605abf0
NoodleSamaChan <lferraraccio@outlook.com>
2023-12-20 16:05:25 +0100

    completed lesson on comments

---
+++ comments/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //hello, world comment
+    println!("Hello, world!");
+
+    //this is a comment
+    //this is a comment on a different line
+    //what a nice series of comments
+
+    let name = "Noodlesama"; //comment at end of line
+    println!("Name is {name}");
+
+}
