839444195ef98f063fed90cb2182d4a55bdf88f6
NoodleSamaChan <150461421+NoodleSamaChan@users.noreply.github.com>
2024-01-02 21:28:31 +0100

    Delete guessing_game directory

--- guessing_game/target/debug/deps/guessing_game-0e30d7a076dc0769.d
+++
@@ -1,5 +0,0 @@
-/Users/lunaferraraccio/projects/guessing_game/target/debug/deps/libguessing_game-0e30d7a076dc0769.rmeta: src/main.rs
-
-/Users/lunaferraraccio/projects/guessing_game/target/debug/deps/guessing_game-0e30d7a076dc0769.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/deps/guessing_game-308822b449ed2eb8.d
+++
@@ -1,5 +0,0 @@
-/Users/lunaferraraccio/projects/guessing_game/target/debug/deps/guessing_game-308822b449ed2eb8: src/main.rs
-
-/Users/lunaferraraccio/projects/guessing_game/target/debug/deps/guessing_game-308822b449ed2eb8.d: src/main.rs
-
-src/main.rs:
