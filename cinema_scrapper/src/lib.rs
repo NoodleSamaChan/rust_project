8732037ca638d461b7a26ac3081a6dc4dc5f0545
NoodleSamaChan <lferraraccio@outlook.com>
2024-02-08 00:01:11 +0100

    added hour comparison

--- cinema_scrapper/src/lib.rs
+++ cinema_scrapper/src/lib.rs
@@ -1,8 +1,32 @@
 extern crate reqwest;
 extern crate scraper;
 use scraper::{Html, Selector};
+use time::Time;
+use time::macros::format_description;
+use time::OffsetDateTime;
 
-pub fn scrape_time_and_title_data(html_file:String) -> Vec<String>{
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct MovieTimes {
+    title: String,
+    times: Vec<time::Time>,
+}
+
+pub fn build_scheduler (title_to_add: String, times_to_add: Vec<time::Time>) -> MovieTimes {
+    MovieTimes{
+        title: title_to_add,
+        times: times_to_add,
+    }
+}
+
+impl MovieTimes {
+    pub fn time_left_till_next_viewing (&self, t_time: u8) -> Option<u8> {
+        self.times.iter().filter(|element| t_time < element.hour()).map(|element|element.hour() - t_time).min()
+    }
+    
+}
+
+pub fn scrape_time_and_title_data(html_file:String) -> Vec<MovieTimes>{
 
     let doc_body = Html::parse_document(&html_file);
 
@@ -10,34 +34,41 @@ pub fn scrape_time_and_title_data(html_file:String) -> Vec<String>{
 
     let titre = Selector::parse("div > div > h4 > a").unwrap(); 
 
-    let mut collection_of_times_titles:Vec<&str>= Vec::new();
+    let mut titles_of_movies: String = String::new();
+    let mut times_of_movies:Vec<time::Time> = Vec::new();
+    let mut vec_strcut_movies: Vec<MovieTimes> = Vec::new();
+    let format = format_description!("[hour]h[minute]");
 
     for element in doc_body.select(&block_selector){
 
         for titre_movie in element.select(&titre){
-            let titre_movie = titre_movie.text().collect::<Vec<_>>();
-            collection_of_times_titles.extend(&titre_movie);
-            println!("{:?}", titre_movie);
-            
+            let titre_movie: String = titre_movie.text().map(|element|element.to_string()).next().unwrap();
+            titles_of_movies = titre_movie;
         }
 
         let time = Selector::parse(".hor").unwrap();
 
         for time in element.select(&time){
-            let times = time.text().collect::<Vec<_>>();
-            collection_of_times_titles.extend(&times);
-            println!("{:?}", times);
+            let times: Vec<time::Time> = time.text().clone().map(|element|(Time::parse(element, &format).expect(element))).collect::<Vec<_>>();
+            times_of_movies.extend(times.clone());
         }
+
+        let movie_sched = build_scheduler(titles_of_movies.clone(), times_of_movies.clone());
+        vec_strcut_movies.push(movie_sched);
+
+        titles_of_movies = String::new();
+        times_of_movies = Vec::new();
+
     }
 
-    let mut final_vector: Vec<String> = Vec::new();
+    return vec_strcut_movies
+}
 
-    for value in collection_of_times_titles{
-        final_vector.push(value.to_string())
+pub fn next_movie(schedule:Vec<MovieTimes>) -> MovieTimes {
+    let t_time = OffsetDateTime::now_utc().hour();
 
-    }
+    schedule.iter().min_by_key(|element|element.time_left_till_next_viewing(t_time)).unwrap().clone()
 
-    return final_vector
 }
 
 #[cfg(test)]
--- cinema_scrapper/src/main.rs
+++ cinema_scrapper/src/main.rs
@@ -3,13 +3,22 @@ extern crate scraper;
 use std::io;
 use std::fs::File;
 use cinema_scrapper::scrape_time_and_title_data;
+use cinema_scrapper::next_movie;
 
 fn main(){
     
     let req = reqwest::get("https://www.cineode.fr/le-vigan-le-palace/horaires/").unwrap().text().unwrap();
-    scrape_time_and_title_data(req);
+    let struct_creator = scrape_time_and_title_data(req);
+    let next_movie_viewing = next_movie(struct_creator);
+
+    println!("{:#?}", next_movie_viewing);
 
     let mut resp = reqwest::get("https://www.cineode.fr/le-vigan-le-palace/horaires/").expect("request failed");
     let mut out = File::create("html_file.html").expect("failed to create file");
     io::copy(&mut resp, &mut out).expect("failed to copy content");
+
+//    Notification::new()
+//        .summary("Oh by the way")
+//        .body(&format!("you can catch {:?} at {:?}", duration))
+//        .schedule(chrono::Utc::now() + duration)?;
 }
