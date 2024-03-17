67dcda0f929d458c314ae2ec19990aca5795fd71
NoodleSamaChan <lferraraccio@outlook.com>
2024-03-17 12:11:31 +0100

    implemented window library

--- grains_de_sable/src/main.rs
+++ grains_de_sable/src/main.rs
@@ -6,97 +6,11 @@ use rand::SeedableRng;
 use minifb::MouseMode;
 use minifb::MouseButton;
 use std::cmp::Reverse;
+use window_rs::WindowBuffer;
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
-
-pub struct WindowBuffer {
-    width: usize,
-    height: usize,
-
-    buffer: Vec<u32>,
-}
-impl WindowBuffer {
-    pub fn new(width: usize, height: usize) -> Self {
-        Self {
-            width,
-            height,
-            buffer: vec![0; width * height],
-        }
-    }
-
-    pub fn width(&self) -> usize {
-        self.width
-    }
-
-    pub fn height(&self) -> usize {
-        self.height
-    }
-
-    pub fn buffer(&self) -> Vec<u32> {
-        self.buffer.clone()
-    }
-
-    pub fn reset(&mut self) {
-        self.buffer.fill(0);
-    }
-}
-impl fmt::Display for WindowBuffer {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-
-        let line_len = self.buffer.chunks(self.width);
-        for i in line_len{
-            for a in i{
-                match a {
-                    0 => f.write_str(".")?,
-                    _ => f.write_str("#")?,
-                    
-                } 
-            }
-            f.write_str("\n")?;  
-        }
-        Ok(())
-    }
-}
-impl std::ops::Index<(usize, usize)> for WindowBuffer {
-    type Output = u32;
-
-    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
-        if x >= self.width {
-            panic!(
-                "Tried to index in a buffer of width {} with a x of {}",
-                self.width, x
-            );
-        }
-        if y >= self.height {
-            panic!(
-                "Tried to index in a buffer of height {} with a y of {}",
-                self.height, y
-            );
-        }
-
-        &self.buffer[y * self.width + x]
-    }
-}
-impl std::ops::IndexMut<(usize, usize)> for WindowBuffer {
-    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
-        if x >= self.width {
-            panic!(
-                "Tried to index in a buffer of width {} with a x of {}",
-                self.width, x
-            );
-        }
-        if y >= self.height {
-            panic!(
-                "Tried to index in a buffer of height {} with a y of {}",
-                self.height, y
-            );
-        }
-
-        &mut self.buffer[y * self.width + x]
-    }
-}
 #[derive(Clone)]
 #[derive(Debug)]
 pub struct Sand {
@@ -267,6 +181,7 @@ pub fn rgb2(red: u8, green: u8, blue: u8) -> u32 {
 mod test {
     use super::*;
     use proptest::prelude::*;
+    use insta::assert_snapshot;
 
     #[test]
     fn test_rgb() {
@@ -278,61 +193,6 @@ mod test {
         assert_eq!(rgb2(0x12, 0x34, 0x56), 0x00_12_34_56);
     }
 
-    #[test]
-    fn display_window_buffer() {
-        let mut buffer = WindowBuffer::new(4, 4);
-        assert_eq!(
-            buffer.to_string(),
-            "....
-....
-....
-....
-"
-        );
-        buffer.buffer[1] = 1;
-        buffer.buffer[3] = 3;
-        buffer.buffer[4] = 4;
-        buffer.buffer[6] = 6;
-        buffer.buffer[9] = 9;
-        buffer.buffer[11] = 11;
-        buffer.buffer[12] = 12;
-        buffer.buffer[14] = 14;
-        assert_eq!(
-            buffer.to_string(),
-            ".#.#
-#.#.
-.#.#
-#.#.
-"
-        );
-    }
-
-    #[test]
-    fn display_window_buffer_v2() {
-        let mut buffer = WindowBuffer::new(4, 4);
-        assert_display_snapshot!(buffer.to_string(), @r###"
-        ....
-        ....
-        ....
-        ....
-        "###);
-
-        buffer.buffer[1] = 1;
-        buffer.buffer[3] = 3;
-        buffer.buffer[4] = 4;
-        buffer.buffer[6] = 6;
-        buffer.buffer[9] = 9;
-        buffer.buffer[11] = 11;
-        buffer.buffer[12] = 12;
-        buffer.buffer[14] = 14;
-        assert_display_snapshot!(buffer.to_string(), @r###"
-        .#.#
-        #.#.
-        .#.#
-        #.#.
-        "###); 
-    }
-
     proptest! {
         #[test]
         fn test_both_rgb(red in 0u8.., green in 0u8.., blue  in 0u8..) {
@@ -341,41 +201,6 @@ mod test {
     }
 
     #[test]
-    #[should_panic]
-    fn test_bad_index_width() {
-        let mut buffer = WindowBuffer::new(4, 4);
-        buffer[(0, 5)] = 0;
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_bad_index_height() {
-        let mut buffer = WindowBuffer::new(4, 4);
-        buffer[(5, 0)] = 0;
-    }
-
-    #[test]
-    fn test_index() {
-        let mut buffer = WindowBuffer::new(4, 4);
-        buffer[(0, 1)] = 1;
-        buffer[(0, 3)] = 3;
-        buffer[(1, 0)] = 4;
-        buffer[(1, 2)] = 6;
-        buffer[(2, 1)] = 9;
-        buffer[(2, 3)] = 11;
-        buffer[(3, 0)] = 12;
-        buffer[(3, 2)] = 14;
-        assert_display_snapshot!(
-            buffer.to_string(),
-            @r###"
-        .#.#
-        #.#.
-        .#.#
-        #.#.
-        "###
-        );
-    }
-    #[test]
     fn simple_sand_drop() {
         let mut buffer = WindowBuffer::new(5, 4);
         let mut world = World {
@@ -383,7 +208,7 @@ mod test {
             colors: Box::new(World::color_generator()),
         };
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         ...#.
@@ -395,7 +220,7 @@ mod test {
 
         world.update(&buffer);
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         .....
@@ -407,7 +232,7 @@ mod test {
 
         world.update(&buffer);
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         .....
@@ -429,7 +254,7 @@ mod test {
 
         world.update(&buffer);
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###""###
         );
@@ -446,7 +271,7 @@ mod test {
             colors: Box::new(World::color_generator()),
         };
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         ..#..
@@ -457,7 +282,7 @@ mod test {
         );
         world.update(&buffer);
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         .....
@@ -468,7 +293,7 @@ mod test {
         );
         world.update(&buffer);
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         .....
@@ -479,7 +304,7 @@ mod test {
         );
         world.update(&buffer);
         world.display(&mut buffer);
-        assert_display_snapshot!(
+        assert_snapshot!(
             buffer.to_string(),
             @r###"
         .....
